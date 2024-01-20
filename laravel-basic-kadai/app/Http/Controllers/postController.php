7d1a6347965d2aa147b5292030647597eb63de77
makirin <143609096+makirin@users.noreply.github.com>
2024-01-20 17:34:19 +0900

    Add files via upload

--- laravel-basic-kadai/app/Http/Controllers/postController.php
+++ laravel-basic-kadai/app/Http/Controllers/postController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 
+use App\Models\Post;
+
 class postController extends Controller {
     public function index() {
         // postsテーブルからすべてのデータを取得し、変数$productsに代入する
@@ -13,5 +15,13 @@ class postController extends Controller {
     
         // 変数$postsをposts/index.blade.phpファイルに渡す
         return view('posts.index', compact('posts'));
-      }    
+      }
+      
+      public function show($id) {
+        // URL'/products/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをproductsテーブルから取得し、変数$productに代入する
+        $post = Post::find($id);
+      
+        // 変数$productをproducts/show.blade.phpファイルに渡す
+        return view('posts.show', compact('post'));
+      }
 }
