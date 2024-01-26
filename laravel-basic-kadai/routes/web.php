0a9cbf7a2271da746b6ed9335662e8821902bf68
makirin <143609096+makirin@users.noreply.github.com>
2024-01-26 21:18:39 +0900

    Add files via upload

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 });
 
 Route::get('/posts', [PostController::class, 'index']);
+
 Route::get('/posts/create', [PostController::class, 'create']);
 
 Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
