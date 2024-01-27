e327d92d6213b5e911a9dda0a686b34943884155
makirin <maki.rincha.aries@gmail.com>
2024-01-27 16:44:48 +0900

    lastComit

--- laravel-basic-kadai/app/Http/Controllers/postController.php
+++ laravel-basic-kadai/app/Http/Controllers/postController.php
@@ -6,12 +6,43 @@ use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\DB;
 
+use App\Models\Post;
+
 class postController extends Controller {
     public function index() {
-        // postsテーブルからすべてのデータを取得し、変数$productsに代入する
+        // postsテーブルからすべてのデータを取得し、変数$postsに代入する
         $posts = DB::table('posts')->get();
     
         // 変数$postsをposts/index.blade.phpファイルに渡す
         return view('posts.index', compact('posts'));
-      }    
+      }
+      
+      public function show($id) {
+        // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$productに代入する
+        $post = Post::find($id);
+      
+        // 変数$productをproducts/show.blade.phpファイルに渡す
+        return view('posts.show', compact('post'));
+      }
+
+      public function create () {
+        return view('posts.create');
+      }
+
+      public function store(Request $request) {
+        // バリデーションを設定する
+        $request->validate([
+          'title' => 'required|max:20',
+          'content' => 'required|max:200'
+        ]);
+
+        // フォームの入力内容をもとに、テーブルにデータを追加する
+        $post = new Post();
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->save();
+  
+        // リダイレクトさせる
+        return redirect("/posts");
+      }
 }
--- laravel-basic-kadai/resources/views/posts/index.blade.php
+++ laravel-basic-kadai/resources/views/posts/index.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="ja">
  
@@ -16,33 +15,10 @@
       <th>本文</th>
     </tr>
     @foreach($posts as $post)
-     <tr>
-      <td>{{ $post->title }}</td>
-      <td>{{ $post->content }}</td>
-     @endforeach       
-     </table>
-=======
-<!DOCTYPE html>
-<html lang="ja">
- 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Laravel基礎課題</title>
-</head>
- 
-<body>
-  <h1>投稿一覧</h1>
-  <table>
     <tr>
-      <th>タイトル</th>
-      <th>本文</th>
-    </tr>
-    @foreach($posts as $post)
-     <tr>
       <td>{{ $post->title }}</td>
       <td>{{ $post->content }}</td>
-     @endforeach       
-     </table>
->>>>>>> origin/main
+    </tr>
+    @endforeach
+  </table>
 </body>
